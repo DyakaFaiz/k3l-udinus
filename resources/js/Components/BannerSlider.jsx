17c6967bb2f0839dc542ddfa5b68c54928934da0
Dyaka Faiz <112964868+DyakaFaiz@users.noreply.github.com>
2026-08-03 08:51:02 +0700

    refactor(photo file): to public_html

--- resources/js/Components/BannerSlider.jsx
+++ resources/js/Components/BannerSlider.jsx
@@ -6,13 +6,13 @@ const BannerSlider = () => {
             id: 1,
             title: 'Latihan Memadamkan Api',
             subtitle: 'Kegiatan simulasi dan pelatihan tanggap darurat pemadaman api menggunakan APAR bagi mahasiswa.',
-            imgUrl: '/storage/photos/fire-1.jpg'
+            imgUrl: '/photos/fire-1.jpg'
         },
         {
             id: 2,
             title: 'Simulasi Tanggap Darurat',
             subtitle: 'Praktek pemadaman api menggunakan APAR yang dipandu langsung oleh instruktur terlatih.',
-            imgUrl: '/storage/photos/fire-2.jpeg'
+            imgUrl: '/photos/fire-2.jpeg'
         }
     ];
 
